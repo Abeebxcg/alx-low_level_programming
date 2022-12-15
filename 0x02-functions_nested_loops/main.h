f7ab1438b1000eba2c15f95a82c6743f1b06d2e8
root <aoolalere@gmail.comgit config --global user.name Your>
2022-12-15 08:29:29 -0800

    my commit

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,23 +1,25 @@
-#include "main.h"
+int _putchar(char c);
 
-/**
-* main - print putchar
-* Return: always 0
-*/
+void print_alphabet(void);
 
-int main(void)
-{
+void print_alphabet_x10(void);
 
-		_putchar('_');
-		_putchar('p');
-		_putchar('u');
-		_putchar('t');
-		_putchar('c');
-		_putchar('h');
-		_putchar('a');
-		_putchar('r');
-		_putchar('\n');	
+int _islower(int c);
 
-		return (0);
+int _isalpha(int c);
 
-}
+int print_sign(int n)
+
+int  _abs(int);
+
+int print_last_digit(int);
+
+void jack_baver(void);
+
+void times_table(void);
+
+void add(int, int);
+
+void print_to_98(int n);
+
+void print_times_table(int n);
