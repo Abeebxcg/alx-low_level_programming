18e4cb2bfc58cd7acb2b56e9dfeb423061ea5529
root <aoolalere@gmail.comgit config --global user.name Your>
2022-12-16 15:28:21 -0800

    d

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,11 +1,21 @@
 #include "main.h"
 
-/*
+/**
 * _isupper - checks if parameter is an uppercase character.
 * @c: input character.
 * Return: 1 if is an uppercase character, 0 in other case.
 */
 int _isupper(int c)
 {
-	return (c >= 'A' && c <= 'Z');
+char uppercase = 'A';
+int isupper = 0;
+for (; uppercase <= 'z'; uppercase++)
+{
+if (c == uppercase)
+{
+isupper = 1;
+break;
+}
+}
+	return (isupper);
 }
